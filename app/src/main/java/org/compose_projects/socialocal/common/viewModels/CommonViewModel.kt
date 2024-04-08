d416e647da5487b7fcd3e6ae03568bdd8c0705ad
Less-parrot <less100100@gmail.com>
2024-04-07 22:44:06 -0500

    agregacion de commonViewModel

--- app/src/main/java/org/compose_projects/socialocal/common/viewModels/CommonViewModel.kt
+++ app/src/main/java/org/compose_projects/socialocal/common/viewModels/CommonViewModel.kt
@@ -2,7 +2,7 @@ package org.compose_projects.socialocal.common.viewModels
 
 import androidx.lifecycle.ViewModel
 
-class ViewModelBase: ViewModel() {
+class CommonViewModel: ViewModel() {
     /*
     viewModelBase es un viewModel que contendrá varias
     funcionalidades que se van a usar a lo largo de todas la aplicación
